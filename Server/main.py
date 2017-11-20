98d06e59cc117032a66846053206dd7b2019bc9c
Hasan Men <hmen.56@gmail.com>
2017-11-20 18:55:43 +0000

    FlaskServer initialized

---
+++ Server/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=['GET'])
+def root():
+	return "Wild Animal Trap Project"
+
+
+
+
+if __name__=="__main__":
+	app.run(host="0.0.0.0",port=5000)
